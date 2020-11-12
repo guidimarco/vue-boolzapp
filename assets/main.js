f844e5ee9f2d2b88919eed95f4ee60391e390d87
Marco Guidi <mark.88g@gmail.com>
2020-11-12 16:27:33 +0100

    HTML-JS generati contatti

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,228 @@
 var app = new Vue({ // VUE INSTANCE
     el: "#root",
     data: { // for var
-
+        userContacts: [ // all user-contacts
+            {
+                name: "Michele",
+                avatar: "img/_1.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Paola",
+                avatar: "img/_2.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Francesco",
+                avatar: "img/_3.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Boolean",
+                avatar: "img/_4.png",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Donato",
+                avatar: "img/_5.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Sofia",
+                avatar: "img/_6.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Marco",
+                avatar: "img/_7.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Francesca",
+                avatar: "img/_8.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Lucia",
+                avatar: "img/_9.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+            {
+                name: "Antonio",
+                avatar: "img/_10.jpg",
+                visible: true,
+                messages: [ // messages
+                    {
+                        data: "10/01/2020 15:30:55",
+                        message: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 15:50:00",
+                        message: "Ricordati di dargli da mangiare",
+                        status: "sent"
+                    },
+                    {
+                        data: "10/01/2020 16:15:22",
+                        message: "Tutto fatto!",
+                        status: "received"
+                    },
+                ] // END messages
+            }, // END contact
+        ]
     },
     methods: { // for function
 
